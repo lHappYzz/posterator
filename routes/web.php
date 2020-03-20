d105cb5145e0c372c348bba2e7e585d5717884fd
Martynenko Oleksandr <48642804+lHappYzz@users.noreply.github.com>
2020-03-20 19:34:07 +0200

    Auth system

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
