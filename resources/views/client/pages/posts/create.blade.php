f522ff30bffa9ac3ad02fd710179ef487519d822
Martynenko Oleksandr <48642804+lHappYzz@users.noreply.github.com>
2020-06-09 14:59:11 +0300

    Update: updated create post page

--- resources/views/client/pages/posts/create.blade.php
+++ resources/views/client/pages/posts/create.blade.php
@@ -1,14 +1,17 @@
-@extends('admin.layouts.app_admin')
+@extends('layouts.app')
 @section('content')
-
-    <div class="container ck-content">
-        @component('admin.components.breadcrumb')
-            @slot('title') Create post @endslot
-            @slot('parent') Main @endslot
-            @slot('middle_pages', ['post.index' => 'Posts'])
-            @slot('active') Create post @endslot
-        @endcomponent
-        <hr>
+    <div class="container">
+        @if(Auth::user()->hasRole('admin'))
+            @component('admin.components.breadcrumb')
+                @slot('title') Create post @endslot
+                @slot('parent') Main @endslot
+                @slot('middle_pages', ['post.index' => 'Posts'])
+                @slot('active') Create post @endslot
+            @endcomponent
+            <hr>
+        @else
+            <h2>Create post</h2>
+        @endif
         <form class="form" method="post" action="{{ route('post.store') }}">
             @csrf
             <div class="form-group">
@@ -16,24 +19,21 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text">Post title</span>
                     </div>
-                    <input type="text" class="form-control" name="post_title" maxlength="200" value="{{ old('post_title') }}" placeholder="Some title for the post">
+                    <input required type="text" class="form-control" name="post_title" maxlength="200" value="{{ old('post_title') }}" placeholder="Some title for the post">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-outline-primary">Create</button>
+                    </div>
                 </div>
                 <div class="input-group d-inline">
                     <textarea name="post_text" id="editor"></textarea>
                 </div>
-                <button type="submit" class="btn btn-outline-primary">Create</button>
             </div>
         </form>
     </div>
 @endsection
-@section('test')
-
-    @endsection
 @push('scripts')
     <script type="text/javascript" src="/public/js/scripts.js?v=26"></script>
-
     <script src="https://cdn.ckeditor.com/ckeditor5/18.0.0/classic/ckeditor.js"></script>
-
     <script>
         ClassicEditor
             .create( document.querySelector( '#editor' ), {
