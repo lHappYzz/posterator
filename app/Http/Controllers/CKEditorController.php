38c0511aadc8c5fbe08a694ac042bb8c0dd79fc2
Martynenko Oleksandr <48642804+lHappYzz@users.noreply.github.com>
2020-06-29 21:52:00 +0300

    CKEditorController code refactoring

--- app/Http/Controllers/CKEditorController.php
+++ app/Http/Controllers/CKEditorController.php
@@ -16,18 +16,22 @@ class CKEditorController extends Controller
         return ($mb > 0) ? $mb : 0;
     }
 
-    /** test
-     * @param String $uploadDir
-     * @param int $year
-     * @param int $month
+    /**
+     * @return string
      */
-    protected function checkDir($uploadDir, $year, $month){
-        if (!file_exists($uploadDir.$year)) {
-            mkdir($uploadDir.$year, 0777, true);
+    protected function makeUploadDir(){
+        $uploadDirectory = public_path() . DIRECTORY_SEPARATOR . "ckeditor" . DIRECTORY_SEPARATOR . "uploads" . DIRECTORY_SEPARATOR;
+
+        $year = date("Y");
+        $month = date("m");
+
+        if (!file_exists($uploadDirectory.$year)) {
+            mkdir($uploadDirectory.$year, 0777, true);
         }
-        if (!file_exists($uploadDir.$year.'/'.$month)) {
-            mkdir($uploadDir.$year.'/'.$month, 0777, true);
+        if (!file_exists($uploadDirectory.$year.'/'.$month)) {
+            mkdir($uploadDirectory.$year.'/'.$month, 0777, true);
         }
+        return $uploadDirectory .= $year . DIRECTORY_SEPARATOR . $month . DIRECTORY_SEPARATOR;
     }
 
     /**
@@ -64,22 +68,18 @@ class CKEditorController extends Controller
                 self::checkExtension($_FILES['upload'], ['jpeg', 'jpg', 'png', 'gif', 'svg', 'bmp']);
                 self::checkSize($_FILES['upload'], 2);
 
-                $uploadDirectory = public_path() . DIRECTORY_SEPARATOR . "ckeditor" . DIRECTORY_SEPARATOR . "uploads" . DIRECTORY_SEPARATOR;
-                $year = date("Y");
-                $month = date("m");
-                self::checkDir($uploadDirectory, $year, $month);
+                $uploadDirectory = self::makeUploadDir();
 
                 $imgType = explode('/', $_FILES['upload']['type'])[1];
                 $tmp_name = $_FILES['upload']['tmp_name'];
 
                 $newName = uniqid() . '.' . $imgType;
-                $uploadDirectory .= $year . DIRECTORY_SEPARATOR . $month . DIRECTORY_SEPARATOR;
 
                 move_uploaded_file($tmp_name, $uploadDirectory . '/' . $newName);
 
                 $response = [
                     'uploaded' => true,
-                    'url' => URL::to('/') . '/public/ckeditor/uploads/'.$year.'/'.$month.'/'.$newName
+                    'url' => URL::to('/') . '/public/ckeditor/uploads/'. date("Y") . '/' . date("m") . '/' . $newName
                 ];
                 return json_encode($response);
             } else {
