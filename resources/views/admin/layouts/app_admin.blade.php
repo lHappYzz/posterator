3a17451d50dcd5c11a26d3cb088b75ab04fd60fc
Martynenko Oleksandr <48642804+lHappYzz@users.noreply.github.com>
2020-07-19 12:28:50 +0300

    logo update

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -25,6 +25,7 @@
     {{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
     <link href="/public/css/app.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="{{ URL::to('/') . '/public/ckeditor/uploads/LogoIconGreen.png' }}" type="image/png">
     @stack('styles')
     <style>
         img {
@@ -43,7 +44,7 @@
     <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="/public/ckeditor/uploads/laravel_logo_white_color.png" width="100" class="d-inline-block align-top" alt="Laravel" loading="lazy">
+                <img src="/public/ckeditor/uploads/LogoTransparentGreen.png" width="150" class="d-inline-block align-top" alt="Laravel" loading="lazy">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
