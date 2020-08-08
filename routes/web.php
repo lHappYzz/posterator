115ae972f0f2e0add4c49fa2703bd04cac2ff522
Martynenko Oleksandr <48642804+lHappYzz@users.noreply.github.com>
2020-08-08 21:40:16 +0300

    Email verification added

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
+Auth::routes(['verify' => true]);
+
 Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth', 'role:admin']], function(){
     Route::get('/', 'DashboardController@index')->name('admin.index');
     Route::resource('/category', 'CategoryController', ['as'=>'admin']);
@@ -37,17 +39,18 @@ Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth', 'r
 
 Route::get('/', 'PagesController@mainPage')->name('user.page.main');
 
-Route::group(['prefix' => 'profile', 'middleware' => 'auth'], function (){
+Route::group(['prefix' => 'profile', 'middleware' => ['auth', 'verified']], function (){
     Route::get('/', 'PagesController@profile')->name('user.page.profile');
     Route::get('/posts', 'ProfileController@userPosts')->name('user.page.profile.posts');
     Route::get('/edit', 'ProfileController@profileEdit')->name('user.page.profile.edit');
     Route::put('/update/{user}', 'ProfileController@update')->name('user.page.profile.update');
+
+    Route::resource('/post', 'PostController')->except('show');
+    Route::post('/post/publish', 'PostController@publish')->name('post.publish');
+    Route::post('/comment', 'PostController@storeComment')->name('comment.store');
+    Route::post('/uploads', 'CKEditorController@uploadImg')->name('image.upload');
 });
 
-Route::resource('/post', 'PostController')->except('show');
 Route::get('/post/{post}', 'PostController@show')->middleware('can.view-post')->name('post.show');
-Route::post('/post/publish', 'PostController@publish')->name('post.publish');
-Route::post('/comment', 'PostController@storeComment')->name('comment.store');
-Route::post('/uploads', 'CKEditorController@uploadImg')->name('image.upload');
 
 
